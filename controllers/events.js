7f8e7f1b72bd03547813b2c829c39d1d6c5cf660
oswaldinho24k <oswaldinho24k@MacBook-Pro-de-Oswaldo.local>
2019-08-13 16:58:27 -0500

    lol

--- controllers/events.js
+++ controllers/events.js
@@ -13,7 +13,7 @@ controller.getEvents = async (req, res) => {
 
 controller.postEvent = async (req, res) => {
 	const {speakers, location, description} = req.body
-	console.log(req.body)
+	
 	
 	if (req.body['speakers']) req.body['speakers'] = JSON.parse(speakers)
 	if (req.body['location']) req.body['location'] = JSON.parse(location)
