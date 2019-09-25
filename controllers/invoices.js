c0d50f88054fe4a7a09c61133c4206396f35d6d6
oswaldinho <oswalfut_96@hotmail.com>
2019-09-25 09:14:48 -0500

    Emisor del CFDi no corresponde al Rfc del Usuario Timbrador.  :(

--- controllers/invoices.js
+++ controllers/invoices.js
@@ -24,26 +24,33 @@ controller.postInvoice=async(req, res)=>{
 
   if (!payment) return res.status(400).json({message:'Sin Orden no podemos facturar!'})  
   if (payment.invoice) return res.status(400).json({message:'Ya se ha facturado este pago'}) 
-  if (!fiscalData.rfc) return res.status(400).json({message:'No es posible facturar sin tu RFC'})    
+  if (!fiscalData.rfc) return res.status(400).json({message:'No es posible facturar sin tu RFC'})
+
+  let total = payment.amount
+  let subTotal = (total / 1.16).toFixed(2).toString()
+  let iva = (subTotal * .16).toFixed(2).toString()
+
+  console.log('payment:', payment)
+  console.log('amgData :', amgData )  
   
  const cfdi = new CFDI({
-   'Serie':payment.paymentType == 'Event' ? amgData.eventSerie : amgData.membershipSerie,
-   'Folio': payment.paymentType == 'Event' ? amgData.eventFolio + 1 : amgData.membershipFolio + 1,
-   'Fecha': new Date().toISOString().split('.')[0],
-   'NoCertificado': amgData.privateNumber,
-   'SubTotal': (payment.amount * .84).toString(),
-   'Moneda': 'MXN',
-   'Total': payment.amount.toString(),
-   'TipoDeComprobante': 'I',
-   'FormaPago': '01',
-   'MetodoPago': 'PUE',
-   'TipoCambio': '1',
-   'LugarExpedicion': '03100',
+  'Serie':payment.paymentType == 'Event' ? amgData.eventSerie : amgData.membershipSerie,
+  'Folio': payment.paymentType == 'Event' ? amgData.eventFolio + 1 : amgData.membershipFolio + 1,
+  'Fecha': new Date().toISOString().split('.')[0],
+  'NoCertificado': amgData.privateNumber,
+  'SubTotal': subTotal,
+  'Moneda': 'MXN',
+  'Total':total.toFixed(2).toString(),
+  'TipoDeComprobante': 'I',
+  'FormaPago': '01',
+  'MetodoPago': 'PUE',
+  'TipoCambio': '1',
+  'LugarExpedicion': amgData.zipCode,
  });
   
  cfdi.cer = 'public/invoice_files/LAN7008173R5.cer.pem'
  cfdi.key = 'public/invoice_files/LAN7008173R5.key.pem'
- cfdi.withOutCerts = true
+ cfdi.withOutCerts = false
   
  cfdi.add(new Emisor({
    'Rfc': amgData.rfc,
@@ -52,8 +59,8 @@ controller.postInvoice=async(req, res)=>{
  }))
     
  cfdi.add(new Receptor({
-   'Rfc': fiscalData.rfc,
-   'Nombre': `${basicData.name || ''} ${basicData.dadSurname || ''} ${basicData.momSurname || ''}`,
+   'Rfc': 'XAXX010101000',  
+  // 'Nombre': `${basicData.name || ''} ${basicData.dadSurname || ''} ${basicData.momSurname || ''}`,
   //  'ResidenciaFiscal': 'MEX',
   //  'NumRegIdTrib': '0000000000000',
    'UsoCFDI': 'G03'
@@ -66,16 +73,16 @@ controller.postInvoice=async(req, res)=>{
    'Cantidad': '1',
    'Unidad': 'SERVICIO',
    'Descripcion': 'MEMBRESIA',
-   'ValorUnitario': payment.amount,
-   'Importe': payment.amount
+   'ValorUnitario': subTotal,
+   'Importe': subTotal
  })
     
  concepto.add(new Traslado({
-   'Base': (payment.amount*.84).toString(),
+   'Base': subTotal,
    'Impuesto': '002',
    'TipoFactor': 'Tasa',
    'TasaOCuota': '0.160000',
-   'Importe': (payment.amount*.16).toString()
+   'Importe': iva
  }))
   
   cfdi.add(concepto)
@@ -84,9 +91,9 @@ controller.postInvoice=async(req, res)=>{
     'Impuesto': '002',
     'TipoFactor': 'Tasa',
     'TasaOCuota': '0.160000',
-    'Importe': (payment.amount*.16).toString()
+    'Importe': iva
   }, {}, {
-    'TotalImpuestosTrasladados': (payment.amount*.16).toString()
+    'TotalImpuestosTrasladados':iva
   }))
   
 
@@ -107,9 +114,110 @@ controller.postInvoice=async(req, res)=>{
 
       return res.status(200).json({xmlString})
     })
-    .catch(e=>res.json(e))
+    .catch(e=>res.json(e))  
+}
+
+controller.postManualInvoice=async(req, res)=>{
+
+  const amgDataFacturacion = await DataFacturacion.find()
+  const amgData = amgDataFacturacion[0]
+  const {amount, rfc, address,  paymentMethod} = req.body
+  //const {paymentId} = req.params
+  // njmvnmjvgfconst payment = await Payment.findById(paymentId)
+
+  if (!amount) return res.status(400).json({message:'Sin Cantidad no podemos facturar!'})  
+  //if (payment.invoice) return res.status(400).json({message:'Ya se ha facturado este pago'}) 
+  if (!rfc) return res.status(400).json({message:'No es posible facturar sin tu RFC'})
+
+  let total = amount
+  let subTotal = (total / 1.16).toFixed(2).toString()
+  let iva = (subTotal * .16).toFixed(2).toString()
+
+  console.log('payment:', payment)
+  console.log('amgData :', amgData )  
+  
+ const cfdi = new CFDI({
+  'Serie':payment.paymentType == 'Event' ? amgData.eventSerie : amgData.membershipSerie,
+  'Folio': payment.paymentType == 'Event' ? amgData.eventFolio + 1 : amgData.membershipFolio + 1,
+  'Fecha': new Date().toISOString().split('.')[0],
+  'NoCertificado': amgData.privateNumber,
+  'SubTotal': subTotal,
+  'Moneda': 'MXN',
+  'Total':total.toFixed(2).toString(),
+  'TipoDeComprobante': 'I',
+  'FormaPago': '01',
+  'MetodoPago': paymentMethod || 'PUE',
+  'TipoCambio': '1',
+  'LugarExpedicion': amgData.zipCode,
+ });
+  
+ cfdi.cer = 'public/invoice_files/LAN7008173R5.cer.pem'
+ cfdi.key = 'public/invoice_files/LAN7008173R5.key.pem'
+ cfdi.withOutCerts = false
+  
+ cfdi.add(new Emisor({
+   'Rfc': amgData.rfc,
+   'Nombre': amgData.name,
+   'RegimenFiscal': amgData.regime
+ }))
+    
+ cfdi.add(new Receptor({
+   'Rfc': 'XAXX010101000',  
+  // 'Nombre': `${basicData.name || ''} ${basicData.dadSurname || ''} ${basicData.momSurname || ''}`,
+  //  'ResidenciaFiscal': 'MEX',
+  //  'NumRegIdTrib': '0000000000000',
+   'UsoCFDI': 'G03'
+ }))
+    
+ const concepto = new Concepto({
+   'ClaveProdServ': '94101602',
+   'ClaveUnidad': 'E48',
+   'NoIdentificacion': '00001',
+   'Cantidad': '1',
+   'Unidad': 'SERVICIO',
+   'Descripcion': 'MEMBRESIA',
+   'ValorUnitario': subTotal,
+   'Importe': subTotal
+ })
+    
+ concepto.add(new Traslado({
+   'Base': subTotal,
+   'Impuesto': '002',
+   'TipoFactor': 'Tasa',
+   'TasaOCuota': '0.160000',
+   'Importe': iva
+ }))
+  
+  cfdi.add(concepto)
+    
+  cfdi.add(new Traslado({
+    'Impuesto': '002',
+    'TipoFactor': 'Tasa',
+    'TasaOCuota': '0.160000',
+    'Importe': iva
+  }, {}, {
+    'TotalImpuestosTrasladados':iva
+  }))
   
 
+ 
+
+  let promises = await Promise.all([getApiToken(), cfdi.getXml()])  
+  const [tokenRes, xml] = promises
+  let buff = Buffer.from(xml)
+  let base64data = buff.toString('base64')
+
+  timbrarCfdi(tokenRes.token, base64data)
+    .then(c =>{
+      if(!c.success) return res.status(400).json(c)
+      console.log(c)
+      let buff = Buffer.from(c.cfdi, 'base64');
+      let xmlString = buff.toString('utf8');
+      //const payment = await Payment.findByIdAndUpdate(req.params.id, { $set: { invoice: xmlString} })
+
+      return res.status(200).json({xmlString})
+    })
+    .catch(e=>res.json(e))  
 }
 
 
