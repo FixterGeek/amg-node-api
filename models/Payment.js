e604bf5ea8ab688e15c0eaf0fadedd76bd83c396
Héctor BlisS <contacto@fixter.org>
2019-10-30 09:03:58 -0600

    users in payments

--- models/Payment.js
+++ models/Payment.js
@@ -2,36 +2,41 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const paymentSchema = new Schema({
-  user:{
-    type:Schema.Types.ObjectId,
-    ref:'User'
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
   },
-  concept:String,
+  users: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  chat: Array,
+  concept: String,
   conektaId: String,
-  date:{
-    type:String,
-    required:true    
+  date: {
+    type: String,
+    required: true
   },
-  amount:{
-    type:Number,
-    required:true
+  amount: {
+    type: Number,
+    required: true
   },
-  paid:{
-    type:Boolean,
-    default:false
+  paid: {
+    type: Boolean,
+    default: false
   },
-  paymentType:{
-    type:String,
-    enum:['Subscription', 'Event']
+  paymentType: {
+    type: String,
+    enum: ['Subscription', 'Event']
   },
-  invoice:String,
-  recipetURL:String,
-  filial:{
-    type:Schema.Types.ObjectId,
-    ref:'Filial'
+  invoice: String,
+  recipetURL: String,
+  filial: {
+    type: Schema.Types.ObjectId,
+    ref: 'Filial'
   }
-},{
-  timestamps:true
+}, {
+  timestamps: true
 })
 
 module.exports = mongoose.model('Payment', paymentSchema)
