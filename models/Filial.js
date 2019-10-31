c3423badb697b7efeb6bd1393736ce37d88d4f42
oswaldinho <oswalfut_96@hotmail.com>
2019-10-30 23:21:04 -0600

    added extra fields to filial

--- models/Filial.js
+++ models/Filial.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const filialSchema = new Schema({
+  photoURL:String,
+  businessName:'String',
   state: {
     type: String,
     unique: true,
