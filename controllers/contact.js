f36172d011de280e227275d65f17cd553d882ee5
oswaldinho <oswalfut_96@hotmail.com>
2019-11-23 17:16:22 -0600

    contact

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,7 +1,8 @@
 const {sendContactMail} = require('../helpers/mailer')
 const controller = {}
 
-controller.sendContactMail = (req, res) => {
+controller.sendContactMail = async (req, res) => {
+  console.log(req.body)
   sendContactMail(req.body)
     .then(r=>res.status('200').json(r))
     .catch(e=>res.status('400').json(e))
--- routes/contact.js
+++ routes/contact.js
@@ -2,12 +2,6 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/contact')
 
-const tryCatch = (fn) => {
-  return (req, res, next) => {
-    return fn(req, res).catch(e => next(e));
-  };
-}
-
-router.post('/', tryCatch(controller.sendContactMail))
+router.post('/', controller.sendContactMail)
 
 module.exports = router
